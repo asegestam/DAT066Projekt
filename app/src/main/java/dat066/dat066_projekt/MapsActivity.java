69ee6799828da4c62a0235df66fc900a11c44f9f
asegestam <abbesegestam@gmail.com>
2018-11-12 21:18:38 +0100

    LocationListener som beräknar avstånd, hastiget

--- app/src/main/java/dat066/dat066_projekt/MapsActivity.java
+++ app/src/main/java/dat066/dat066_projekt/MapsActivity.java
@@ -1,7 +1,18 @@
 package dat066.dat066_projekt;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,9 +21,21 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback{
 
     private GoogleMap mMap;
+    private static final String TAG = "MapsActivity";
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+    private Boolean mLocationPermissionsGranted = false;
+
+
+    static Location lastLocation = null;
+    static double distanceInMetres;
+    LocationManager locationManager;
+    LocationListener locationListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +45,49 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-    }
+        distanceInMetres = 0;
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                if(lastLocation == null) {
+                    lastLocation = location;
+                }
+                long currentTime = location.getTime();
+                long lastTime = lastLocation.getTime();
+                long timeBetween = (currentTime - lastTime)/1000;
+                double distance = location.distanceTo(lastLocation);
+                if(distanceInMetres > 21100) {
+                    //nån bugg där efter första 10m så hoppar den upp med 21183 m
+                    distanceInMetres += distance - 21183;
+                }
+                distanceInMetres += distance;
+                double speed = distance/timeBetween;
+                //Toast.makeText(MapsActivity.this, "Distance " + distanceInMetres, Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "Distance " + distanceInMetres + " m");
+                Log.d(TAG, "Time " + timeBetween + " s");
+                Log.d(TAG, "Speed " + speed + " m/s");
+                Log.d(TAG, "Hastighet " + (speed*3600)/1000 + " km/h");
+                lastLocation = location;
+            }
 
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+        getLocationPermission();
+    }
 
     /**
      * Manipulates the map once available.
@@ -42,5 +106,65 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+    }
+
+    /**Gets the necessary permissions from the user or asks for them*/
+    private void getLocationPermission() {
+        Log.d(TAG, "getLocationPermission: getting location permissions");
+        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION};
+
+        //Check the permissions fine_location and coarse_location from the user
+        //Ask for the coarse_location permission because it was not already granted
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(), FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(this.getApplicationContext(), COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                mLocationPermissionsGranted = true;
+                //Every permission is granted, initialize the map
+                initMap();
+                locationManager.requestLocationUpdates( LocationManager.GPS_PROVIDER, 1000, 10, locationListener);
+            } else {
+                //Ask for the fine_location permission because it was not already granted
+                ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        } else
+            ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.d(TAG, "onRequestPermissionsResult: Checking the results of the permission requests");
+        switch (requestCode) {
+            case LOCATION_PERMISSION_REQUEST_CODE: {
+                if (grantResults.length > 0) {
+
+                    for (int grantResult : grantResults) {
+                        if (grantResult != PackageManager.PERMISSION_GRANTED) {
+
+                            Log.d(TAG, "onRequestPermissionsResult: permission failed");
+                            mLocationPermissionsGranted = false;
+                            return;
+                        }
+                    }
+                    Log.d(TAG, "onRequestPermissionsResult: permission granted");
+                    mLocationPermissionsGranted = true;
+                    //All permissions are granted so we can initialize the map
+                    initMap();
+                }
+            }
+        }
+    }
+
+    /**Initializes the map*/
+    private void initMap() {
+        Log.d(TAG, "initMap: initializing the map");
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+
+        assert mapFragment != null;
+        mapFragment.getMapAsync(MapsActivity.this);
     }
 }
