fb18e34edad81ac581137b41f230eccc81eb7341
vdde <36074206+vdde@users.noreply.github.com>
2018-11-15 16:45:45 +0100

    Lagt till lite extra kring att spara informationen

--- app/src/main/java/dat066/dat066_projekt/MapsActivity.java
+++ app/src/main/java/dat066/dat066_projekt/MapsActivity.java
@@ -27,6 +27,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
     private Boolean mLocationPermissionsGranted = false;
+    private String gender;
+    private int weight;
+    private int age;
 
 
     @Override
@@ -40,14 +43,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         getLocationPermission();
     }
 
+    /**
+     *  Gets input from screen and saves it with shared preferences
+     *
+     *  Vi kan nog flytta detta om vi ska ha våra kalorieuträkningar
+     *  i en annan klass
+     */
     public void Saveinfo(View view){
         SharedPreferences sharedPref = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPref.edit();
         editor.putString("name", null/*input*/);
         editor.putString("gender", null/*input*/);
-        editor.putString("weight", null/*input*/);
-        editor.putString("age", null/*input*/);
+        editor.putInt("weight", 0/*input*/);
+        editor.putInt("age", 0/*input*/);
         editor.apply();
+
+    }
+    /**
+     * Gets the user information from userInfo.xml and stores it in three variables
+     * 
+     *  Vi kan nog flytta detta om vi ska ha våra kalorieuträkningar
+     *  i en annan klass
+     */
+    public void getInfo(){
+        SharedPreferences sharedPref = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+        gender = sharedPref.getString("gender", "");
+        weight = sharedPref.getInt("weight", 0);
+        age = sharedPref.getInt("age", 0);
 
     }
 
