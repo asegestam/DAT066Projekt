aff6839264c46fc9f3b61a4ceb2560235464d650
asegestam <abbesegestam@gmail.com>
2018-11-20 14:45:30 +0100

    Fragment Refaktorerings bös
    En liten bugg då man byter fragment då tråden i mapfragment körs

--- app/src/main/java/dat066/dat066_projekt/ProfileFragment.java
+++ app/src/main/java/dat066/dat066_projekt/ProfileFragment.java
@@ -7,19 +7,26 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class ProfileFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View v = inflater.inflate(R.layout.fragment_profile, container, false);
+        EditText weightET = (EditText) (v.findViewById(R.id.weight));
+        return v;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
             super.onViewCreated(view, savedInstanceState);
-            
             getActivity().setTitle("Profile-Fragment");
+
     }
+
 }
